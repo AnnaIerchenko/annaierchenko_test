302f3be39cb0b67171a33792150e0f80a8d793d8
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2025-06-11 09:44:26 -0700

    fix: log for see env

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import './app/styles/global.css';
 import { App } from './app/App';
 
+if (import.meta.env.DEV) {
+    console.log('VITE_GITHUB_API:', import.meta.env.VITE_GITHUB_API);
+}
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
