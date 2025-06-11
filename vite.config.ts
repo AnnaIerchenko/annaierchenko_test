12b8deebc8501fe48933a2a79b41e9c9e539cf0b
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2025-06-11 09:08:08 -0700

    build: update vite config to load env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 // import { defineConfig } from 'vitest/config';
 import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src')}},
+  define: {
+    'import.meta.env': {
+      ...env
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom', 
     setupFiles: './src/setupTests.ts', 
-    exclude: [...configDefaults.exclude, 'e2e'], // если e2e у тебя отдельно
-  },
-})
+    exclude: [...configDefaults.exclude, 'e2e'], 
+    },
+  };
+});
